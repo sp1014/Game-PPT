26848e846e0f5e955940ff1b87c1ca414e3e1493
Santiago Perez <80767619+sp1014@users.noreply.github.com>
2021-10-24 12:55:19 -0500

    salucion-error-salir del juego

--- main.go
+++ main.go
@@ -43,4 +43,61 @@ Introduce tu nombre:
 		fmt.Println("Has Perdido")
 	}
 
+	i := 0
+	fmt.Print("Seleccione [0] para salir o [1] para seguir jugando  \n")
+	fmt.Scanln(&i)
+	switch i {
+	case 0:
+		fmt.Println("Fin del Juego")
+	case 1:
+		fmt.Println("Nueva Partida")
+
+		nombre :=
+			`
+Bienvenido al juego de cara y sello 
+
+Introduce tu nombre:
+
+`
+		fmt.Print(nombre)
+		fmt.Scanln(&nombre)
+
+		fmt.Println("Tu nombre es", nombre)
+
+		menu :=
+			`
+	Elije
+	[ 0 ] Cara
+	[ 1 ] Sello
+	¿Qué prefieres?
+`
+		fmt.Print(menu)
+
+		var eleccion int
+		fmt.Scanln(&eleccion)
+
+		rand.Seed(time.Now().UnixNano())
+		min := 0
+		max := 1
+		resultado := rand.Intn(max-min+1) + min
+
+		if resultado == eleccion {
+			fmt.Println("Has ganado")
+		} else {
+			fmt.Println("Has Perdido")
+		}
+
+		i := 0
+		fmt.Print("Seleccione [0] para salir o [1] para seguir jugando  \n")
+		fmt.Scanln(&i)
+		switch i {
+		case 0:
+			fmt.Println("one")
+		case 1:
+			fmt.Println("two")
+
+		}
+
+	}
+
 }
